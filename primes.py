86246fb2102071fcecc404859801341950b18d54
Serge Alkhalil <syraj.alkhalil7@gmail.com>
2022-09-28 22:40:03 +0100

    solution

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    i = 0
+    while len(list) < number_of_primes:
+        if isPrime(i):
+            list.append(i)
+        i += 1
     return list
+
+def isPrime(number):
+    if number == 0 or number == 1:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
